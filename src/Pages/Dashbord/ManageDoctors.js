a76ad5f0ce606c1de680b522072f8fc5409895d3
sharifulislam <shariful0401@gmail.com>
2022-05-31 21:38:57 +0600

    added doctor delete part

--- src/Pages/Dashbord/ManageDoctors.js
+++ src/Pages/Dashbord/ManageDoctors.js
@@ -2,19 +2,43 @@ import { is } from 'date-fns/locale';
 import React from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../Home/Shared/Loading';
+import DoctorRow from './DoctorRow';
 
 const ManageDoctors = () => {
-  const {data: doctors, isLoading} = useQuery('doctor', () => fetch('http://localhost:5000/doctor',{
-    headers:{
+  const { data: doctors, isLoading , refetch} = useQuery('doctor', () => fetch('http://localhost:5000/doctor', {
+    headers: {
       authorization: `Bearer ${localStorage.getItem('accessToken')}`
     }
   }).then(res => res.json()))
-  if(isLoading){
+  if (isLoading) {
     return <Loading></Loading>
   }
   return (
     <div>
       <h2 className='text-2xl'>Manage Doctors:{doctors.length}</h2>
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Avatar</th>
+              <th>Name</th>
+              <th>Specialty</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+           {
+             doctors.map((doctor, index )=> <DoctorRow
+             key={doctor._id}
+             index={index}
+             doctor={doctor}
+             refetch={refetch}
+             ></DoctorRow>)
+           }
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
